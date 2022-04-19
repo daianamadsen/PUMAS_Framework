f219b4356b4e5f8fadf8dad441fd06a9d3407ccf
Daiana <daianamadsen@gmail.com>
2022-04-19 20:06:39 -0300

    α * UAC + β * (1 - FAA) + γ * FCA + 𝛿 * RAC  >=  UAA

--- src/edu/isistan/pumas/framework/protocols/commons/userAgents/proposalAcceptanceStrategy/ProposalAcceptanceStrategyStrict.java
+++ src/edu/isistan/pumas/framework/protocols/commons/userAgents/proposalAcceptanceStrategy/ProposalAcceptanceStrategyStrict.java
@@ -7,13 +7,46 @@ import edu.isistan.pumas.framework.protocols.commons.userAgents.UserAg;
 
 public class ProposalAcceptanceStrategyStrict<T extends SURItem> implements ProposalAcceptanceStrategy<T> {
 
+	private double pf_alpha, pf_beta, pf_gamma, pf_delta;
+	
+	public void setPfBeta(double pf_beta){
+		this.pf_beta = pf_beta;
+	}
+	
+	public void setPfGamma(double pf_gamma){
+		this.pf_gamma = pf_gamma;
+	}
+	
+	public void setPfDelta(double pf_delta){
+		this.pf_delta = pf_delta;
+	}
+
 	@Override
 	public boolean accepts(UserAg<T> ag, AgProposal<T> p) {
 		try {
-			double af = (ag.getAssertivenessFactor() > 0.0) ? ag.getAssertivenessFactor() : 1.0; //1.0 if not initialized
-			double cf = (ag.getCooperativenessFactor() > 0.0) ? ag.getCooperativenessFactor() : 1.0; //1.0 if not initialized
-			double rf = (ag.getRelationshipsFactor().get(p.getAgentID()) != null) ? ag.getRelationshipsFactor().get(p.getAgentID()) : 1.0; //1.0 if not initialized
-			return (ag.getUtilityFor(p)*cf*rf >= ag.getUtilityFor(ag.getCurrentProposal())*af);
+			//Initialize Personality Factors
+			pf_alpha = 1;
+			double af = ag.getAssertivenessFactor();
+			if (af >= 0.0) {
+				pf_alpha = pf_alpha - pf_beta;
+			} else {
+				pf_beta = 0;
+			}
+			double cf = ag.getCooperativenessFactor();
+			if (cf >= 0.0) {
+				pf_alpha = pf_alpha - pf_gamma;
+			} else {
+				pf_gamma = 0;
+			}
+			double rf = 0;
+			if (ag.getRelationshipsFactor().get(p.getAgentID()) != null) {
+				pf_alpha = pf_alpha - pf_delta;
+				rf = ag.getRelationshipsFactor().get(p.getAgentID());
+			} else {
+				pf_delta = 0;
+			}
+			double otherProposal = pf_alpha*ag.getUtilityFor(p) + pf_beta*(1-af) + pf_gamma*cf + pf_delta*rf;
+			return (otherProposal >= ag.getUtilityFor(ag.getCurrentProposal()));
 		} catch (NothingToProposeException e) {
 			//if i'm here => ag.getCurrentProposal() threw an exception => 
 			//he will always agree to other agent's proposal as he doesn't have anything to propose
