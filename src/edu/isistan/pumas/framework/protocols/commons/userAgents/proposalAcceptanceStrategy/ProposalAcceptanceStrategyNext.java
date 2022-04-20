2f65a505081924a7e8bb9b4c3112c9b28f912d23
Daiana <daianamadsen@gmail.com>
2022-04-20 17:37:08 -0300

    α * UAC + β * (1 - FAA) + γ * FCA + 𝛿 * RAC  >=  UAA

--- src/edu/isistan/pumas/framework/protocols/commons/userAgents/proposalAcceptanceStrategy/ProposalAcceptanceStrategyNext.java
+++ src/edu/isistan/pumas/framework/protocols/commons/userAgents/proposalAcceptanceStrategy/ProposalAcceptanceStrategyNext.java
@@ -7,6 +7,20 @@ import edu.isistan.pumas.framework.protocols.commons.userAgents.UserAg;
 
 public class ProposalAcceptanceStrategyNext <T extends SURItem> extends ProposalAcceptanceStrategyStrict<T>{
 
+	private double pf_alpha, pf_beta, pf_gamma, pf_delta;
+	
+	public void setPfBeta(double pf_beta){
+		this.pf_beta = pf_beta;
+	}
+	
+	public void setPfGamma(double pf_gamma){
+		this.pf_gamma = pf_gamma;
+	}
+	
+	public void setPfDelta(double pf_delta){
+		this.pf_delta = pf_delta;
+	}
+
 	@Override
 	public boolean accepts(UserAg<T> ag, AgProposal<T> p) {
 		if (super.accepts(ag, p)) 
@@ -14,10 +28,29 @@ public class ProposalAcceptanceStrategyNext <T extends SURItem> extends Proposal
 		else {
 			try {
 				AgProposal<T> nextProp = ag.peekNextProposal();
-				double af = (ag.getAssertivenessFactor() > 0.0) ? ag.getAssertivenessFactor() : 1.0; //1.0 if not initialized
-				double cf = (ag.getCooperativenessFactor() > 0.0) ? ag.getCooperativenessFactor() : 1.0; //1.0 if not initialized
-				double rf = (ag.getRelationshipsFactor().get(p.getAgentID()) != null) ? ag.getRelationshipsFactor().get(p.getAgentID()) : 1.0; //1.0 if not initialized
-				return (ag.getUtilityFor(p)*cf*rf >= ag.getUtilityFor(nextProp)*af); //if p is better than my next proposal => accept
+				//Initialize Personality Factors
+				pf_alpha = 1;
+				double af = ag.getAssertivenessFactor();
+				if (af >= 0.0) {
+					pf_alpha = pf_alpha - pf_beta;
+				} else {
+					pf_beta = 0;
+				}
+				double cf = ag.getCooperativenessFactor();
+				if (cf >= 0.0) {
+					pf_alpha = pf_alpha - pf_gamma;
+				} else {
+					pf_gamma = 0;
+				}
+				double rf = 0;
+				if (ag.getRelationshipsFactor().get(p.getAgentID()) != null) {
+					pf_alpha = pf_alpha - pf_delta;
+					rf = ag.getRelationshipsFactor().get(p.getAgentID());
+				} else {
+					pf_delta = 0;
+				}
+				double otherProposal = pf_alpha*ag.getUtilityFor(p) + pf_beta*(1-af) + pf_gamma*cf + pf_delta*rf;
+				return (otherProposal >= ag.getUtilityFor(nextProp)); //if p is better than my next proposal => accept
 				
 			} catch (NonConcedableCurrentProposalException e) {
 				return false; //there is no next proposal (currentProposal can't be conceded) && p is not better than my current proposal (because of the first "if" clause)
--- src/edu/isistan/pumas/framework/protocols/commons/userAgents/proposalAcceptanceStrategy/ProposalAcceptanceStrategyRelaxedS.java
+++ src/edu/isistan/pumas/framework/protocols/commons/userAgents/proposalAcceptanceStrategy/ProposalAcceptanceStrategyRelaxedS.java
@@ -56,13 +56,46 @@ public class ProposalAcceptanceStrategyRelaxedS<T extends SURItem> implements Pr
 		this.relaxationLevel = relaxationLevel;
 	}
 
+	private double pf_alpha, pf_beta, pf_gamma, pf_delta;
+	
+	public void setPfBeta(double pf_beta){
+		this.pf_beta = pf_beta;
+	}
+	
+	public void setPfGamma(double pf_gamma){
+		this.pf_gamma = pf_gamma;
+	}
+	
+	public void setPfDelta(double pf_delta){
+		this.pf_delta = pf_delta;
+	}
+
 	@Override
 	public boolean accepts(UserAg<T> ag, AgProposal<T> p) {
 		try {
-			double af = (ag.getAssertivenessFactor() > 0.0) ? ag.getAssertivenessFactor() : 1.0; //1.0 if not initialized
-			double cf = (ag.getCooperativenessFactor() > 0.0) ? ag.getCooperativenessFactor() : 1.0; //1.0 if not initialized
-			double rf = (ag.getRelationshipsFactor().get(p.getAgentID()) != null) ? ag.getRelationshipsFactor().get(p.getAgentID()) : 1.0; //1.0 if not initialized
-			return (ag.getUtilityFor(p)*cf*rf >= ag.getUtilityFor(ag.getCurrentProposal())*af - this.relaxationLevel.getRelaxLevelValue());
+			//Initialize Personality Factors
+			pf_alpha = 1;
+			double af = ag.getAssertivenessFactor();
+			if (af >= 0.0) {
+				pf_alpha = pf_alpha - pf_beta;
+			} else {
+				pf_beta = 0;
+			}
+			double cf = ag.getCooperativenessFactor();
+			if (cf >= 0.0) {
+				pf_alpha = pf_alpha - pf_gamma;
+			} else {
+				pf_gamma = 0;
+			}
+			double rf = 0;
+			if (ag.getRelationshipsFactor().get(p.getAgentID()) != null) {
+				pf_alpha = pf_alpha - pf_delta;
+				rf = ag.getRelationshipsFactor().get(p.getAgentID());
+			} else {
+				pf_delta = 0;
+			}
+			double otherProposal = pf_alpha*ag.getUtilityFor(p) + pf_beta*(1-af) + pf_gamma*cf + pf_delta*rf;
+			return (otherProposal >= ag.getUtilityFor(ag.getCurrentProposal()) - this.relaxationLevel.getRelaxLevelValue());
 		} catch (NothingToProposeException e) {
 			//if i'm here => ag.getCurrentProposal() threw an exception => 
 			//he will always agree to other agent's proposal as he doesn't have anything to propose
