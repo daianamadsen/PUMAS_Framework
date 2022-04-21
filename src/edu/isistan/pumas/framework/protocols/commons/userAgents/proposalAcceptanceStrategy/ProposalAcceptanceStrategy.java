03e7fe310dd03b94171b64657238c5fb540b17f3
Daiana <daianamadsen@gmail.com>
2022-04-21 20:51:05 -0300

    PERSONALITY_FACTORS PARAMETERS

--- src/edu/isistan/pumas/framework/protocols/commons/userAgents/proposalAcceptanceStrategy/ProposalAcceptanceStrategy.java
+++ src/edu/isistan/pumas/framework/protocols/commons/userAgents/proposalAcceptanceStrategy/ProposalAcceptanceStrategy.java
@@ -5,6 +5,10 @@ import edu.isistan.pumas.framework.protocols.commons.proposal.AgProposal;
 import edu.isistan.pumas.framework.protocols.commons.userAgents.UserAg;
 
 public interface ProposalAcceptanceStrategy <T extends SURItem>{
+	
+	public final double PROPOSAL_ACCEPTANCE_PF_BETA = 0.25;
+	public final double PROPOSAL_ACCEPTANCE_PF_GAMMA = 0.25;
+	public final double PROPOSAL_ACCEPTANCE_PF_DELTA = 0.25;
 
 	public boolean accepts (UserAg<T> ag, AgProposal<T> p);
 	
